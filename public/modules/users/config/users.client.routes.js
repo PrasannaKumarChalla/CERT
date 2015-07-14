e9fce6bdf726ad8dcf9662359e94fef523557eee
S521942 <kumar.challa7893@gmail.com>
2015-07-14 00:09:19 -0500

    Added server control for lists

--- public/modules/users/config/users.client.routes.js
+++ public/modules/users/config/users.client.routes.js
@@ -5,6 +5,10 @@ angular.module('users').config(['$stateProvider',
 	function($stateProvider) {
 		// Users state routing
 		$stateProvider.
+		state('admin', {
+			url: '/admin',
+			templateUrl: 'modules/users/views/admin.client.view.html'
+		}).
 		state('profile', {
 			url: '/settings/profile',
 			templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
